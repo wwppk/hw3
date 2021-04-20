f6cb979e02a09b80b675361b536a789316bb89b8
Konstantin <navrotskiy.k@gmail.com>
2021-04-20 09:11:33 +0300

    Initial commit

---
+++ hello-py/app.py
@@ -0,0 +1,24 @@
+import os
+import json
+
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route("/health")
+def health():
+    return '{"status": "ok"}'
+
+
+@app.route("/version")
+def version():
+    return '{"version": "1"}'
+
+
+@app.route("/")
+def hello():
+    return 'Hello world!!!' #from '+ os.environ['HOSTNAME'] + '!'
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0',port='8000')
