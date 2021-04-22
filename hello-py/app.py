01d051c7801be40a05012c5820abeefe9eb713ca
Konstantin <navrotskiy.k@gmail.com>
2021-04-22 10:11:30 +0300

    1.1

--- hello-py/app.py
+++ hello-py/app.py
@@ -1,9 +1,15 @@
 import os
 import json
 
-from flask import Flask
+from flask import Flask, url_for
+
 app = Flask(__name__)
 
+config = {
+    'DATABASE_URI': os.environ.get('DATABASE_URI', ''),
+    'HOSTNAME': 'HostName', #os.environ['HOSTNAME'],
+    'GREETING': os.environ.get('GREETING', 'Hello'),
+}
 
 @app.route("/health")
 def health():
@@ -19,6 +25,29 @@ def version():
 def hello():
     return 'Hello world!!!' #from '+ os.environ['HOSTNAME'] + '!'
 
+@app.route("/user", methods=['POST'])
+def create_user():
+    return 'user create' #from '+ os.environ['HOSTNAME'] + '!'
+
+@app.route("/user/<userid>")
+def get_user(userid):
+    return 'user ' + userid  #from '+ os.environ['HOSTNAME'] + '!'
+
+@app.route('/db')
+def db():
+    from sqlalchemy import create_engine
+
+    engine = create_engine(config['DATABASE_URI'], echo=True)
+    rows = []
+    with engine.connect() as connection:
+        result = connection.execute("select id, name from client;")
+        rows = [dict(r.items()) for r in result]
+    return json.dumps(rows)
+
 
 if __name__ == "__main__":
+    # print(get_user('111'))
     app.run(host='0.0.0.0',port='8000')
+    # with app.test_request_context():
+    #     print(url_for('get_user', userid = '1'))
+    #     print(url_for('create_user'))
